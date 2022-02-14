eeff4aedd0963fd4ec3ff4fac6fc4c902415d759
Rus <mamail>
2022-02-14 17:38:51 +0200

    +docs folder

---
+++ docs/js/script.js
@@ -0,0 +1,46 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close'),
+      workItems = document.querySelectorAll('.portfolio__item'),
+      workItemsInfo = document.querySelectorAll('.portfolio__item-info');;
+        
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const percents = document.querySelectorAll('.skills__progress-percent'),
+    lines = document.querySelectorAll('.skills__progress-line');
+
+percents.forEach( (item,i) => {
+    lines[i].style.width = item.innerHTML;
+});
+
+workItems.forEach((item, i) => {
+    item.addEventListener("mouseover", () => {
+        workItemsInfo[i].classList.add('show');
+        /* workItemsInfo[i].classList.remove('hide'); */
+    });
+    item.addEventListener("mouseout", () => {
+        /* workItemsInfo[i].classList.add('hide'); */
+        workItemsInfo[i].classList.remove('show');
+    });
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    } 
+});
+
+$("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
